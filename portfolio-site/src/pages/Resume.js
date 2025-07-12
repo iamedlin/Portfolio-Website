63fe50248052e33d13a927dae1d7757ea9bcbb86
iamedlin <naseedielyn@gmail.com>
2025-07-12 21:11:30 +0800

    Update Resume.js

--- portfolio-site/src/pages/Resume.js
+++ portfolio-site/src/pages/Resume.js
@@ -112,16 +112,6 @@ const Resume = () => {
 
     </main>
   );
- /* <main style={{ padding: '40px' }}>
-    <h2>Projects</h2>
-    <div>
-      <h3>Project One</h3>
-      <p>Details about the project.</p>
-    </div>
-    <div>
-      <h3>Project Two</h3>
-      <p>More details here.</p>
-    </div>
-  </main>*/
+
 };
 export default Resume;
